ae56fd9162fa96fe75b86beaf37aa382f0dc6956
kasselTrankos <alvaro.touzon@gmail.com>
2020-10-11 15:06:58 +0200

    feat: next step made concat with name

--- obtain-files.js
+++ obtain-files.js
@@ -3,26 +3,12 @@ import { I } from './lambda';
 import { pipe, getFiles, B, ap, log, chain, filter, map, lift2, flatMap, isDirectory, prop } from './utils';
 import RoseTree from './fp/monad/rosetree';
 
-const traverse = T => xs => xs.reduce((acc, x)=> lift2(append)(x)(acc), T.of([])) 
-const append = x => xs => [x, ...xs]; 
-const dir = RoseTree.of('.');
-export const files = pipe(
-  // traverse(RoseTree),
+const obtainDirs = pipe(
   map(RoseTree.of),
   filter(isDirectory),
-  log('Alv'),
   getFiles
 );
-const getNode = prop('node');
-const setPath = s => `${s}/`;
-
-export const obtainFiles = x => console.log('obtainFiles', x) || x.chain(xs => new RoseTree(xs, files(xs)));
-
 
 export const proc = pipe(
-  obtainFiles
+  chain(xs => new RoseTree(xs, obtainDirs(xs)))
 );
-
-// const h = dir.map(c => console.log(c, '00000'))
-// const k = proc(dir);
-// log('K::: os ')(k)
--- read.js
+++ read.js
@@ -1,17 +1,11 @@
 // read is
-import { log, ap, pipe, map } from './utils';
-import { I, S } from './lambda';
-import {proc, obtainFiles, files} from './obtain-files';
+import { log } from './utils';
+import {proc} from './obtain-files';
 import RoseTree from './fp/monad/rosetree';
 
 const dir = RoseTree.of('.')
-const t = pipe(
-  ap(new RoseTree(I, [ RoseTree.of(c)])),
-  proc
-);
 
-const m = dir.concat(proc(dir))
-const j = m.ap(new RoseTree(x => typeof x === 'string' ? x : proc(x), []))
+const j = proc(dir).ap(RoseTree.of(x => typeof x === 'string' ? x : proc(x)))
 log('M')(JSON.stringify(j))
 
 
