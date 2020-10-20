d61e71b7f48555be4b78e1d93c709b8ea68c8152
kasselTrankos <alvaro.touzon@gmail.com>
2020-10-20 23:59:20 +0200

    feat: reflexion of lambda

--- lambda-ex.js
+++ lambda-ex.js
@@ -1,9 +1,15 @@
 //lambda.js is
-import {K, KI, C, M, I, OR, AND } from './lambda';
+import {K, KI, C, M, I, OR, AND, EQ, NOT, V } from './lambda';
 Function.prototype.toJsBool = function()  { return this(true)(false); }
 
-const NOT = C;
-const ifElse = I;
 const tt = AND(K)(K)('si')('no')
-console.log(tt);
+const e = EQ(K)(KI);
+
+const _4 = NOT(AND(K)(K))(4)(9)
+const _42 = K(I)(42);
+const pair = a => b => f =>  f(a)(b);
+const p = V('ab')('bc');
+const EMPTY = (() => {});
+const l123 = V(1)(pair(2)(pair(3)(EMPTY)));
+console.log(_4, _42(90), p(K(I)), l123(K(I))(K(I))(K));
 
