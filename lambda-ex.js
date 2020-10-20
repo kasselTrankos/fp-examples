336e0ca654fc3158cca9972c2126cb52f5f56d4a
kasselTrankos <alvaro.touzon@gmail.com>
2020-10-20 22:50:52 +0200

    feat: lamdas done

--- lambda-ex.js
+++ lambda-ex.js
@@ -1,2 +1,9 @@
-// lambda-ex.js is
-const {T}  
+//lambda.js is
+import {K, KI, C, M, I, OR, AND } from './lambda';
+Function.prototype.toJsBool = function()  { return this(true)(false); }
+
+const NOT = C;
+const ifElse = I;
+const tt = AND(K)(K)('si')('no')
+console.log(tt);
+
--- lambda/index.js
+++ lambda/index.js
@@ -2,12 +2,20 @@
 const ZERO = f => x => x;
 
 // T = K := ƛxy.x
-const K = x => y => x;
+export const K = x => y => x;
+
 // T := ƛxy.x
-const T = x => y => x;
+export const T = x => y => x;
 
 // F = KI : = ƛxy.y
-const KI = x => y => y;
+export const KI = x => y => y;
+
+// M : = ƛx.xx
+export const M = a => a(a);
+
+// C : = ƛfxy.fyx ( as flip)
+export const C = f => a => b => f(b)(a);
+
 // F : = ƛxy.y
 const F = x => y => y;
 // ONCE := ƛfx.fx
@@ -16,7 +24,7 @@ const ONCE = f => x => f(x);
 const SUCC = num => f => x => f(num(f)(x));
 
 // ADD := ƛnk.n(SUCC)k
-const ADD = n => k => n(SUCC)(k);
+export const ADD = n => k => n(SUCC)(k);
 // B := λab.a∘b = compose
 const B = a => b => x =>a(b(x));
 
@@ -27,14 +35,14 @@ const MULT = B;
 const NOT = b => b(F)(T)
 
 // AND := ƛpq.pTq
-const AND = p => q => p(q)(F);
+export const AND = p => q => p(q)(F);
 
 
 // OR := ƛpq.pT.q
-const OR = p => q => p(T)(q);
+export const OR = p => q => p(T)(q);
 
 // I := ƛx.x
-const I = x => x;
+export const I = x => x;
 
 
 // is0 := ƛn.n(KF)T
@@ -47,6 +55,3 @@ const S = f => g => x => f(x)(g(x));
 
 const PAIR = a => b => f(a)(b);
 
-module.exports = {
-    ZERO, SUCC, ONCE, B, PAIR, S, I
-}
